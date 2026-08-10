99676f998808f54bdbf1a9121f9c182db8b46923
Rauneet Raj <119717249+raunet234@users.noreply.github.com>
2026-08-10 06:15:51 +0530

    Time: 15 ms (64.49%), Space: 42.4 MB (83.55%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+
+        boolean[] dp = new boolean[n + 1];
+
+        // dp[0] = false
+        // No stones -> current player cannot move -> loses
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j * j <= i; j++) {
+
+                int remaining = i - j * j;
+
+                if (!dp[remaining]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
