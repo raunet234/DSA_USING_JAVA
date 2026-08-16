dd316dd81d0b15174ffa2ebe721c29f98974359c
Rauneet Raj <119717249+raunet234@users.noreply.github.com>
2026-08-16 18:46:04 +0530

    Time: 7 ms (29.05%), Space: 114.7 MB (64.86%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        // Count stones based on remainder when divided by 3
+        for (int stone : stones) {
+
+            if (stone % 3 == 0) {
+                count0++;
+            } 
+            else if (stone % 3 == 1) {
+                count1++;
+            } 
+            else {
+                count2++;
+            }
+        }
+
+        // Even number of 0-remainder stones
+        if (count0 % 2 == 0) {
+            return count1 > 0 && count2 > 0;
+        }
+
+        // Odd number of 0-remainder stones
+        return Math.abs(count1 - count2) > 2;
+    }
+}
